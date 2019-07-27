aacd3088d7389d4a811cca2e057d8c15e663e20d
Ian Ross <ianhowesross@gmail.com>
2019-07-26 21:24:52 -0400

    Setting up database

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
