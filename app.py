afb2c63028654042f3f92f1f7e734a3ff5b5dee3
Ian Ross <ianhowesross@gmail.com>
2019-07-28 02:44:26 -0400

    POST will also display time

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html')
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == "__main__":
